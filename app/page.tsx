8dc9093d81704a2423f049f0e90f24bd66bc1d0c
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-10-26 12:22:09 +0530

    shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -25,6 +26,7 @@ export default function Home() {
               priority
             />
           </a>
+          <Button>Shadcn</Button>
         </div>
       </div>
 

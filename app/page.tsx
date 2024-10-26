a6e6eedd5cdf7615215248caabdfb999d91a9e30
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-10-26 13:14:44 +0530

    banner(custom gradient bg)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import BgGradient from "@/components/common/bg-gradient";
+import Banner from "@/components/home/banner";
+
 export default function Home() {
 	return (
 		<main className="mx-auto inset-0 h-full bg-[radial-gradient(#e5e7eb_1px, transparent_1px)] [background-size:16px_16px]">
-			hello
+			<BgGradient />
+      <Banner />
 		</main>
 	);
 }

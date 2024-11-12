e99d647a923afb0ea4abf98ed0cbd1dcb8a702c9
kirtiraj22 <kirtirajthakor064@gmail.com>
2024-11-12 15:24:02 +0530

    upload thing core

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter} from "uploadthing/next"
+import { UploadThingError} from "uploadthing/server"
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+    videoOrAudioUploader: f({ video: {
+        maxFileSize: "32MB"
+    }}).middleware(async ({ req }) => {
+        const user = await currentUser();
+        console.log( { user })
+
+        if(!user) {
+            throw new UploadThingError("Unauthorized");
+        }
+
+        return {
+            userId: user.id
+        }
+    }).onUploadComplete(async ({ metadata, file }) => {
+        console.log("Upload complete for userId: ", metadata.userId)
+        console.log("file url: ", file.url)
+
+        return {
+            userId: metadata.userId,
+            file
+        }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
